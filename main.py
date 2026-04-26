a9fbd4443346c70ae8fdc446bee598220091a29d
bubua12 <wangty334752@gmail.com>
2026-04-26 11:17:29 +0800

    feat: 添加包管理等测试代码

---
+++ main.py
@@ -0,0 +1,4 @@
+from my_pkg import math_operations
+
+result_add = math_operations.add(3, 5)
+print(result_add)
---
+++ my_pkg/app.py
@@ -0,0 +1,4 @@
+import math_operations
+
+add_result = math_operations.add(3, 5)
+print(add_result)
